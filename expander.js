a75038f746b4832dcf575aaec249bdba8adfed85
Ryan Zec <code@ryanzec.com>
2013-06-06 22:28:36 -0400

    Added controller directive

--- expander.js
+++ expander.js
@@ -4,6 +4,34 @@ angular.module('nag.expander', [])
   function($timeout){
     return {
       restrict: 'A',
+      priority: 1000,
+      controller: [
+          '$scope',
+          function($scope) {
+            $scope.contentVisible = false;
+
+            this.expand = function() {
+              $scope.contentVisible = true;
+            }
+
+            this.collapse = function() {
+              $scope.contentVisible = false;
+            }
+
+            this.toggle = function() {
+              $scope.contentVisible = !$scope.contentVisible;
+            }
+
+            Object.defineProperty(this, 'contentVisible', {
+                get: function() {
+                    return $scope.contentVisible;
+                },
+                set: function(value) {
+                    $scope.contentVisible = value;
+                }
+            })
+          }
+      ],
       compile: function(element, attributes, transclude) {
         $(element).addClass('nag-expander');
 
@@ -13,7 +41,7 @@ angular.module('nag.expander', [])
         return function(scope, element, attributes) {
           scope.contentVisible = false;
 
-          //$timeout used in case the data attribute it added dynamically (like with the nucleus angular attribute directive
+          //$timeout used in case the data attribute is added dynamically (like with the nucleus angular attribute directive
           $timeout(function(){scope.contentVisible = $(element).data('default-expand')}, 0);
         }
       }
