9bb490e2cf0f19d0ec0ee1aec9f1b3550b6422e9
Ryan Zec <code@ryanzec.com>
2013-06-19 07:28:09 -0400

    Added style option with a valid value of button-drop-down (to replace the button-drop-down component)

--- expander.js
+++ expander.js
@@ -1,44 +1,77 @@
-angular.module('nag.expander', [])
+angular.module('nag.expander', [
+  'nag.core'
+])
 .directive('nagExpander', [
   '$timeout',
-  function($timeout){
+  'nagDefaults',
+  function($timeout, nagDefaults){
     return {
       restrict: 'A',
       priority: 1000,
+      scope: {
+        options: '=?nagExpander',
+        model: '=?'
+      },
       controller: [
-          '$scope',
-          function($scope) {
-            $scope.contentVisible = false;
+        '$scope',
+        function($scope) {
+          $scope.contentVisible = false;
 
-            this.expand = function() {
-              $scope.contentVisible = true;
-            }
-
-            this.collapse = function() {
-              $scope.contentVisible = false;
-            }
-
-            this.toggle = function() {
-              $scope.contentVisible = !$scope.contentVisible;
-            }
-
-            Object.defineProperty(this, 'contentVisible', {
-                get: function() {
-                    return $scope.contentVisible;
-                },
-                set: function(value) {
-                    $scope.contentVisible = value;
-                }
-            })
+          this.expand = function() {
+            $scope.contentVisible = true;
           }
+
+          this.collapse = function() {
+            $scope.contentVisible = false;
+          }
+
+          this.toggle = function() {
+            $scope.contentVisible = !$scope.contentVisible;
+          }
+
+          Object.defineProperty(this, 'contentVisible', {
+            get: function() {
+              return $scope.contentVisible;
+            },
+            set: function(value) {
+              $scope.contentVisible = value;
+            }
+          })
+        }
       ],
       compile: function(element, attributes, transclude) {
-        $(element).addClass('nag-expander');
+        element.addClass('nag-expander');
 
-        $(element).find('.handle').attr('ng-click', 'contentVisible = !contentVisible');
-        $(element).find('.content').attr('ng-show', 'contentVisible');
+        element.find('.handle').attr('ng-click', 'contentVisible = !contentVisible');
+        element.find('.handle').attr('ng-class', "{'is-active': contentVisible}");
+        element.find('.content').attr('ng-show', 'contentVisible');
 
         return function(scope, element, attributes) {
+          scope.options = nagDefaults.getExpanderOptions(scope.options);
+
+          if(scope.options.style) {
+            element.addClass(scope.options.style);
+
+            if(scope.options.style === 'button-drop-down') {
+
+              var copyingClasses = [
+                'small',
+                'large',
+                'largest',
+                'split',
+                'shadow',
+                'border',
+                'square'
+              ];
+
+              for(var x = 0; x <= copyingClasses.length; x += 1) {
+                if(element.find('button').hasClass(copyingClasses[x])) {
+                  element.addClass(copyingClasses[x]);
+                }
+              }
+            }
+          }
+
           scope.contentVisible = false;
 
           //$timeout used in case the data attribute is added dynamically (like with the nucleus angular attribute directive
