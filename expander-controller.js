f9baf4f17044d36f51098b123aa9d531245473a2
Ryan Zec <code@ryanzec.com>
2014-08-21 18:09:59 -0400

    adding missing file from last commit

---
+++ expander-controller.js
@@ -0,0 +1,63 @@
+angular.module('nag.extendText')
+.controller('NagExpanderDCtrl', [
+  '$scope',
+  function($scope) {
+    $scope.contentVisible = false;
+
+    /**
+     * Expand the element
+     *
+     * @ngdirectivecontroller
+     * @method expand
+     */
+    this.expand = function() {
+      $scope.contentVisible = true;
+    };
+
+    /**
+     * Collapse the element
+     *
+     * @ngdirectivecontroller
+     * @method collapse
+     */
+    this.collapse = function() {
+      $scope.contentVisible = false;
+    };
+
+    /**
+     * Switch the state of the element
+     *
+     * @ngdirectivecontroller
+     * @method toggle
+     */
+    this.toggle = function() {
+      $scope.contentVisible = !$scope.contentVisible;
+    };
+
+    /**
+     * Whether or not the element's content is visible
+     *
+     * @ngdirectivecontroller
+     * @property contentVisible
+     * @type {boolean}
+     */
+    Object.defineProperty(this, 'contentVisible', {
+      get: function() {
+        return $scope.contentVisible;
+      },
+      set: function(value) {
+        $scope.contentVisible = value;
+      }
+    });
+
+    //for compatibility with the single panel directive
+    /**
+     * Adds compatibility with the single panel directive by alias .hide() to .collapse
+     *
+     * @ngdirectivecontroller
+     * @method hide
+     */
+    this.hide = this.collapse;
+    this.show = this.expand;
+  }
+]);
